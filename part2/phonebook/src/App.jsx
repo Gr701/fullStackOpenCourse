ca2bac5f5c07bc2efe0e273cce74946bb89325c1
Gr701 <72044825+Gr701@users.noreply.github.com>
2024-07-11 02:24:15 +0300

    part2 task 2.11 phonebook added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Filter = ({searchValue, handleSearchChange}) => 
   <div>
@@ -26,15 +27,18 @@ const Persons = ({persons, filterPersons}) =>
     <Person key={person.id} person={person}/>)
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-1234567', id: 1},
-    {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-    {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-    {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchValue, setSearchValue] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3002/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
